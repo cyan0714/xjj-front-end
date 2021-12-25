4066f50a895f56522c8f06a671baacbd6200f5b5
chenshiyan <917244295@qq.com>
2021-12-25 10:44:19 +0800

    新增附件、滚动条、轮播图组件

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -5,26 +5,27 @@ module.exports = {
   '@vuepress/pwa': {
     serviceWorker: true,
     updatePopup: {
-      message: "发现新内容可用.",
-      buttonText: "刷新"
-    }
+      message: '发现新内容可用.',
+      buttonText: '刷新',
+    },
   },
   '@vuepress/back-to-top': true,
   '@vuepress/google-analytics': {
-    'ga': secretKeyConf.ga
+    ga: secretKeyConf.ga,
   },
   '@vuepress/medium-zoom': {
     selector: '.content__default img',
   },
   '@vuepress/last-updated': {
-    transformer: (timestamp) => {
-      moment.locale('zh-CN')
+    transformer: timestamp => {
+      moment.locale('zh-CN');
       const isBefore30D = moment(timestamp).isBefore(moment().subtract(30, 'days'), 'day');
       return isBefore30D ? moment(timestamp).format(' YYYY年 MM月 DD日 HH:mm:ss') : moment(timestamp).fromNow();
-    }
+    },
   },
-  "vuepress-plugin-auto-sidebar": {
-    titleMode: "uppercase"
+  'vuepress-plugin-auto-sidebar': {
+    titleMode: 'uppercase',
   },
-  "vuepress-plugin-code-copy":true
+  'vuepress-plugin-code-copy': true,
+  'demo-container': true,
 };
