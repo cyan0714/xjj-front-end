c7b5aa93156f6d252557fbf23d7b1b7729279a09
chenshiyan <917244295@qq.com>
2021-12-28 09:17:16 +0800

    添加常见轮播图

---
+++ docs/.vuepress/components/com/carousel3.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-carousel :interval="4000" type="card" height="200px" arrow="always">
+    <el-carousel-item v-for="item in 6" :key="item">
+      <h3 class="medium">{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+<style scoped lang="scss">
+  ::v-deep .el-carousel__item {
+    h3 {
+      color: #475669;
+      font-size: 18px;
+      opacity: 0.75;
+      line-height: 200px;
+      margin: 0;
+      text-align: center;
+    }
+  }
+  ::v-deep .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  ::v-deep .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+  }
+  ::v-deep .el-carousel__arrow {
+    background-color: rgba(0, 0, 0, 0);
+    &:hover {
+      background-color: rgba(0, 0, 0, 0);
+    }
+  }
+  ::v-deep .el-icon-arrow-right,
+  ::v-deep .el-icon-arrow-left {
+    font-size: 40px;
+    color: #007aff;
+  }
+</style>
